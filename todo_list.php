3a66934589f4d39e59ca45f48a496ff64b49f2e5
Andrew Samaniego <andrewsamaniego@A-S.local>
2014-05-16 14:22:09 -0500

    added new functions to list from 5.16.14 exercise

--- todo_list.php
+++ todo_list.php
@@ -1,35 +1,63 @@
 <?php
 
 // Create array to hold list of todo items
-$items = array();
-array_unshift($items, "");
-unset($items[0]);
+$items = array(TODO item 1, TODO item 2);
+
+// List array items formatted for CLI
+function list_items($list)
+{
+    // Return string of list items separated by newlines.
+    // Should be listed [KEY] Value like this:
+    // [1] TODO item 1
+    // [2] TODO item 2 - blah
+    // DO NOT USE ECHO, USE RETURN
+
+    $result = '';
+    // loop through the list
+    // foreach or for
+     foreach ($list as $key => $value)
+     {
+        $result .= $key . PHP_EOL;
+     }
+
+     return $result;
+}
+
+echo list_items($items);
+$result .= $key . PHP_EOL;
+// Get STDIN, strip whitespace and newlines, 
+// and convert to uppercase if $upper is true
+function get_input($upper = false) 
+{
+    $result = (trim(fgets(STDIN));
+    return $upper ? strtoupper($result): $result;
+}
+get_input();
+get_input(true);
+
 // The loop!
 do {
-    // Iterate through list items
-    foreach ($items as $key => $item) {
-        // Display each item and a newline
-        echo "[{$key}] {$item}\n";
-    }
+    // Echo the list produced by the function
+    echo list_items($items);
 
     // Show the menu options
     echo '(N)ew item, (R)emove item, (Q)uit : ';
 
     // Get the input from user
     // Use trim() to remove whitespace and newlines
-    $input = trim(mb_strtoupper(fgets(STDIN)));
+    $input = get_input(TRUE);
 
     // Check for actionable input
     if ($input == 'N') {
         // Ask for entry
         echo 'Enter item: ';
         // Add entry to list array
-        $items[] = trim(fgets(STDIN));
+        $items[] = get_input();
     } elseif ($input == 'R') {
         // Remove which item?
         echo 'Enter item number to remove: ';
         // Get array key
-        $key = trim(fgets(STDIN));
+        $key = get_input();
         // Remove from array
         unset($items[$key]);
     }
